6d3d7a5c9e892e948ad4672c3b1694d06926d7f1
zxk-afz <zakaryfofana@proton.me>
2024-11-14 14:04:19 -0500

    feat: overlay

--- src/app/lightbox/components/overlay.tsx
+++ src/app/lightbox/components/overlay.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Overlay = (props: { isClickable?: boolean; overlayClassname?: string }) => {
+const Overlay = (props: {
+  isClickable?: boolean;
+  opacity: number;
+  overlayClassname?: string;
+}) => {
   return (
     <div
-      className={`absolute top-0 left-0 w-screen h-screen min-w-screen min-h-screen ${props.overlayClassname}`}
+      style={{ opacity: props.opacity / 100 }}
+      className={`absolute top-0 left-0 w-screen h-screen min-w-screen min-h-screen bg-black ${props.overlayClassname}`}
     />
   );
 };
 
-export default Overlay;
+export default Overlay
--- src/app/lightbox/lightbox.tsx
+++ src/app/lightbox/lightbox.tsx
@@ -3,28 +3,32 @@ import Overlay from "./components/overlay";
 import Frame from "./components/frame";
 import Image from "./components/image";
 
-
 const Lightbox = ({
   title,
   src,
   isOverlayClickable = true,
   overlayClassName,
-  theme = 'dark' 
+  theme = "dark",
+  opacity = 60,
 }: {
   title: string;
   src: string;
   isOverlayClickable?: boolean;
   overlayClassName?: string;
-  theme?: 'light' | 'dark';
+  theme?: "light" | "dark";
+  opacity?: number;
 }) => {
-
   return (
     <div className="aboslute z-50">
-      <Overlay isClickable={isOverlayClickable} overlayClassname={overlayClassName} />
+      <Overlay
+        isClickable={isOverlayClickable}
+        overlayClassname={overlayClassName}
+        opacity={opacity}
+      />
       <Frame title={title} theme={theme}>
         <Image src={src} />
       </Frame>
-    </div> 
+    </div>
   );
 };
 
