2c9a9f3a42214df59fcd44bfd624920d9882c960
zxk-afz <zakaryfofana@proton.me>
2024-11-18 10:50:06 -0500

    fix: unused imports

--- src/app/lightbox/lightbox.tsx
+++ src/app/lightbox/lightbox.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { ReactNode, useState, useEffect } from "react";
-import Image, { StaticImageData } from "next/image";
+import { StaticImageData } from "next/image";
 
 // Lightbox
 const Lightbox = ({
